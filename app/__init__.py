5ca6d235420003596f1fd9ca7b77cc98903fe852
jhonat23 <jhonat23@gmail.com>
2022-09-30 12:09:52 -0500

    first templates created

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from app.config import Config
+from flask_bootstrap import Bootstrap
 
 def create_app():
 
     app = Flask(__name__)
+
+    bootstrap = Bootstrap(app)
 
     app.config.from_object(Config)
 
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from app import create_app
+from flask import render_template
 
 app = create_app()
 
 @app.route('/')
 def root():
-    return 'Hi everyone'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
