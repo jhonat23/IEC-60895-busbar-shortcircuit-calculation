85f64c5032f2163a5c3a4a16c0642ad8cff574c1
jhonat23 <jhonat23@gmail.com>
2022-10-03 18:15:46 -0500

    templates and forms created

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from random import choices
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SelectField, SubmitField
+from wtforms.validators import DataRequired
+
+#forms
+class SystemForm(FlaskForm):
+    project_title = StringField('Project title', validators=[DataRequired()])
+    system_voltage = SelectField('System Voltage (V)', validators=[DataRequired()], choices=['120', '220', '440', '480'], default='440')
+    system_frecuency = SelectField('System frecuency (Hz)', validators=[DataRequired()], choices=['60', '50'], default='60')
+    shortcircuit_current = IntegerField('Shortcircuit current (kA)', validators=[DataRequired()])
+    submit = SubmitField('Next step (2)')
+
+class BusbarForm(FlaskForm):
+    facing_type = SelectField('Facing type', validators=[DataRequired()], choices=['Witdh faced', 'Thickness faced'])
+    busbar_width = IntegerField('Busbar width w (mm)', validators=[DataRequired()], default='0')
+    busbar_thickness = IntegerField('Busbar thickness t (mm)', validators=[DataRequired()], default='0')
+    phase_distance = IntegerField('Phase distance a (mm)', validators=[DataRequired()], default='0')
+    submit = SubmitField('Next step (3)')
+
+class DisposalForm(FlaskForm):
+    span_number = SelectField('Number of spans', validators=[DataRequired()], choices=['1', '2', '3 o more'])
+    support_distance = IntegerField('Support distance (mm)', validators=[DataRequired()], default='0')
+    submit = SubmitField('Next step (Results)')
--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from app import create_app
-from flask import render_template
+from flask import render_template, redirect, make_response
+from app.forms import SystemForm, BusbarForm, DisposalForm
 
 app = create_app()
 
 @app.route('/')
 def root():
+    response = make_response(redirect('/home'))
+    return response
+
+@app.route('/home')
+def home():
     return render_template('index.html')
+
+@app.route('/step_one', methods=['GET', 'POST'])
+def step_one():
+    system_form = SystemForm()
+    context = {
+        'system_form': system_form
+    }
+    return render_template('stepone.html', **context)
+
+@app.route('/step_two', methods=['GET', 'POST'])
+def step_two():
+    busbar_form = BusbarForm()
+    context = {
+        'busbar_form': busbar_form
+    }
+    return render_template('steptwo.html', **context)
+
+@app.route('/step_three', methods=['GET', 'POST'])
+def step_three():
+    disposal_form = DisposalForm()
+    context = {
+        'disposal_form': disposal_form
+    }
+    return render_template('stepthree.html', **context)
+
+@app.route('/results', methods=['GET', 'POST'])
+def results():
+
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
